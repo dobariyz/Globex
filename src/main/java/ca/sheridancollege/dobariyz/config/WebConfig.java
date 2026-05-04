5f1cbeae4080085efa148a6e50e978a3ecf46784
Zeel Dobariya <dobariyz@sheridancollege.ca>
2026-05-04 13:51:50 -0400

    Configure CORS origins from environment

--- src/main/java/ca/sheridancollege/dobariyz/config/WebConfig.java
+++ src/main/java/ca/sheridancollege/dobariyz/config/WebConfig.java
@@ -1,16 +1,21 @@
 package ca.sheridancollege.dobariyz.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.util.StringUtils;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
+    @Value("${app.cors.allowed-origins:http://localhost:5173}")
+    private String allowedOrigins;
+
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:5173")
+                .allowedOrigins(StringUtils.commaDelimitedListToStringArray(allowedOrigins))
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*");
     }
--- src/main/java/ca/sheridancollege/dobariyz/controllers/MarketDataController.java
+++ src/main/java/ca/sheridancollege/dobariyz/controllers/MarketDataController.java
@@ -5,7 +5,6 @@ import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,7 +15,6 @@ import ca.sheridancollege.dobariyz.services.MarketDataService;
 
 @RestController
 @RequestMapping("/api/markets")
-@CrossOrigin(origins = "http://localhost:5173")
 public class MarketDataController {
     
     @Autowired
